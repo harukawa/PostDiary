0f99e02be9e3c94efc5dadf7ff9e13c7e1219c2d
harukawa <harukawa4k@gmail.com>
2019-07-13 21:26:30 +0900

    Change file to sqlite database
    Modify list view and change data management to sqlite.

---
+++ app/src/main/java/com/github/harukawa/postdiary/DatabaseHolder.kt
@@ -0,0 +1,140 @@
+package com.github.harukawa.postdiary
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import java.util.*
+
+class DatabaseHolder(val context: Context){
+    companion object {
+        val ENTRY_TABLE_NAME = "diary"
+    }
+    private val TAG = "PostDiary"
+    private val DATABASE_NAME = "diary.db"
+    private val DATABASE_VERSION = 3
+
+    val dbHelper : SQLiteOpenHelper by lazy {
+        object : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+            override fun onCreate(db: SQLiteDatabase) {
+                db.execSQL("CREATE TABLE " + ENTRY_TABLE_NAME + " ("
+                        + "_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + "FILE TEXT,"
+                        + "TITLE TEXT,"
+                        + "BODY TEXT,"
+                        + "IS_POST INTEGER"
+                        + ");");
+            }
+
+            fun recreate(db: SQLiteDatabase) {
+                db.execSQL("DROP TABLE IF EXISTS $ENTRY_TABLE_NAME")
+                onCreate(db)
+            }
+
+            override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+                Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+                recreate(db);
+            }
+        }
+    }
+    val database by lazy {
+        dbHelper.writableDatabase
+    }
+
+    fun close() {
+        dbHelper.close()
+    }
+}
+
+
+class SelectBuilder(val tableName: String) {
+    var distinct = false
+    var columns = arrayOf<String>()
+    var selection : String? = null
+    var selectionArgs = arrayOf<String>()
+    var groupBy : String? = null
+    var having : String? = null
+    var orderBy : String? = null
+
+    var limit : String? = null
+
+    fun select(vararg fields: String) {
+        columns = arrayOf(*fields)
+    }
+
+    fun order(sentence: String) {
+        orderBy = sentence
+    }
+
+    fun where(whereSentence: String, vararg args: String) {
+        selection = whereSentence
+        selectionArgs = arrayOf(*args)
+    }
+
+
+    fun exec(db: SQLiteDatabase) : Cursor {
+        val columnsArg = if(columns.isEmpty()) null else columns
+        val selectionArgsArg = if(selectionArgs.isEmpty()) null else selectionArgs
+
+        return db.query(distinct, tableName, columnsArg, selection, selectionArgsArg, groupBy, having, orderBy, limit)
+    }
+}
+
+fun DatabaseHolder.query(tableName: String, body: SelectBuilder.()->Unit) : Cursor{
+    val builder = SelectBuilder(tableName)
+    builder.body()
+    return builder.exec(this.database)
+}
+
+fun DatabaseHolder.insertEntry(file: String = "",title: String, body: String, isPost: Int) {
+    val values = ContentValues()
+    values.put("FILE", title)
+    values.put("TITLE", title)
+    values.put("BODY", body)
+    values.put("IS_POST", isPost)
+
+    this.database.insert(DatabaseHolder.ENTRY_TABLE_NAME, null, values)
+}
+
+fun DatabaseHolder.updateEntry(id: Int, file: String, title: String, body: String, isPost: Int) {
+    val (date, _) = getEntry(id)
+    val values = ContentValues()
+    values.put("FILE", file)
+    values.put("TITLE", title)
+    values.put("BODY", body)
+    values.put("IS_POST", isPost)
+    database.update(DatabaseHolder.ENTRY_TABLE_NAME, values, "_id=?", arrayOf(id.toString()))
+}
+
+fun DatabaseHolder.deleteEntries(ids: List<Long>) {
+    ids.forEach {
+        database.delete(DatabaseHolder.ENTRY_TABLE_NAME, "_id=?", arrayOf(it.toString()))
+    }
+}
+
+inline fun <reified T> Cursor.withClose(body: Cursor.()->T) : T{
+    val res = body()
+    this.close()
+    return res
+}
+
+fun DatabaseHolder.getEntry(id: Int): Pair<String, String> {
+    return query(DatabaseHolder.ENTRY_TABLE_NAME) {
+        where("_id=?", id.toString())
+    }.withClose{
+        moveToFirst()
+        Pair(this.getString(1), this.getString(3))
+    }
+}
+
+fun DatabaseHolder.getEntryFile(fileName: String): Pair<String, String> {
+    return query(DatabaseHolder.ENTRY_TABLE_NAME) {
+        where("FILE=?",fileName)
+    }.withClose{
+        moveToFirst()
+        Pair(this.getString(1), this.getString(3))
+    }
+}
--- app/src/main/java/com/github/harukawa/postdiary/ListActivity.kt
+++ app/src/main/java/com/github/harukawa/postdiary/ListActivity.kt
@@ -1,70 +1,120 @@
 package com.github.harukawa.postdiary
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.PostProcessor
+import android.database.Cursor
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class ListActivity : AppCompatActivity() {
 
-    var fileDrafts: MutableList<String> = mutableListOf()
-    var filePosts: MutableList<String> = mutableListOf()
+    val database by lazy { DatabaseHolder(this) }
+
+    var articleDraftsList: MutableList<Column> = mutableListOf()
+    var articlePostsList: MutableList<Column> = mutableListOf()
+    var articleDrafts: MutableList<String> = mutableListOf()
+    var articlePosts: MutableList<String> = mutableListOf()
+
+    val listViewDraft: ListView
+            by lazy { findViewById<ListView>(R.id.listDrafts) }
+    val listViewPost: ListView
+            by lazy { findViewById<ListView>(R.id.listPosts) }
+
+    val adapterPost: ArrayAdapter<String>
+            by lazy { ArrayAdapter(this, android.R.layout.simple_list_item_1, articlePosts) }
+    val adapterDraft: ArrayAdapter<String>
+            by lazy { ArrayAdapter(this, android.R.layout.simple_list_item_1, articleDrafts) }
+
+    val SELECT_FIELDS = arrayOf("_id", "TITLE")
+
+    data class Column(val id: Int, val title: String)
+
+    private fun queryCursor(isPost: Int): Cursor {
+        return database.query(DatabaseHolder.ENTRY_TABLE_NAME) {
+            select(*SELECT_FIELDS)
+            where("IS_POST=?", isPost.toString())
+            order("_id ASC")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        var fileDraftsList = fileList()
+        articleDraftsList = readData(0)
+        articlePostsList = readData(1)
 
-        fileDraftsList.forEach {  fileDrafts.add(it) }
-        fileDraftsList.forEach {  filePosts.add(it) }
+        articleDraftsList.forEach { articleDrafts.add("*" + it.title) }
+        articlePostsList.forEach { articlePosts.add(it.title) }
 
-        val listViewDraft: ListView = findViewById<ListView>(R.id.listDrafts)
-        val listViewPost: ListView = findViewById<ListView>(R.id.listPosts)
-
-        val adapterDrafts: ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, fileDrafts)
-        val adapterPosts: ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, filePosts)
-
-        listViewDraft.adapter = adapterDrafts
-        listViewPost.adapter = adapterPosts
-
-        val a = listViewDraft
+        listViewDraft.adapter = adapterDraft
+        listViewPost.adapter = adapterPost
 
         listViewDraft.setOnItemClickListener() { _, _ /* view */, position, _ /* id */ ->
-            createEditActivity(fileDrafts[position].toString())
+            createEditActivity(articleDraftsList[position].id, 0)
         }
 
         listViewPost.setOnItemClickListener() { _, _ /* view */, position, _ /* id */ ->
-            createEditActivity(filePosts[position].toString())
+            createEditActivity(articlePostsList[position].id, 1)
         }
     }
 
-    val editText = 1
+    val editText = 112
 
-    fun createEditActivity(fileName : String) {
+    fun createEditActivity(id: Int = -1, isPost: Int = -1) {
         val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("EDIT_FILENAME", fileName)
-        startActivityForResult(intent,editText)
+        intent.putExtra("EDIT_ID", id)
+        intent.putExtra("ISPOST", isPost)
+        startActivityForResult(intent, editText)
+    }
+
+    fun readData(isPost: Int): MutableList<Column> {
+        val cursor = queryCursor(isPost)
+        val data: MutableList<Column> = mutableListOf()
+        if (cursor.moveToFirst()) {
+            while (cursor.isAfterLast == false) {
+                val title = cursor.getString(cursor.getColumnIndex("TITLE"))
+                val id = cursor.getInt(cursor.getColumnIndex("_id"))
+                val col: Column = Column(id, title)
+                data.add(col)
+                cursor.moveToNext()
+            }
+        }
+        return data
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == editText) {
-            var fileDraftsList = fileList()
-            fileDraftsList.forEach {  fileDrafts.add(it) }
-            fileDraftsList.forEach {  filePosts.add(it) }
+            articleDrafts.clear()
+            articlePosts.clear()
+            articleDraftsList = readData(0)
+            articlePostsList = readData(1)
+            articleDraftsList.forEach { articleDrafts.add("*" + it.title) }
+            articlePostsList.forEach { articlePosts.add(it.title) }
+            adapterDraft.notifyDataSetChanged()
+            adapterPost.notifyDataSetChanged()
+            listViewDraft.setSelection(listViewDraft.count - 1)
+            listViewPost.setSelection(listViewPost.count - 1)
+        }
+    }
 
-            val listViewDraft: ListView = findViewById<ListView>(R.id.listDrafts)
-            val listViewPost: ListView = findViewById<ListView>(R.id.listPosts)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.list_menu, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
 
-            val adapterDrafts: ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, fileDrafts)
-            val adapterPosts: ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, filePosts)
-
-            listViewDraft.adapter = adapterDrafts
-            listViewPost.adapter = adapterPosts
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_new -> {
+            createEditActivity()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
         }
     }
 }
